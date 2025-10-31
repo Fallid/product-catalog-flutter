9d618ef4d8c9334647da2b3583b23b820b93f003
Ahmad Naufal <naufalopal002@gmail.com>
2025-10-31 21:51:21 +0700

    feat(service): implement get product by id and get product by search query

--- lib/core/services/product_service.dart
+++ lib/core/services/product_service.dart
@@ -6,7 +6,6 @@ class ProductService {
   final Dio _dio;
 
   ProductService(this._dio);
-
   Future<List<Product>> getProducts() async {
     try {
       final resp = await _dio.get(ApiEndpoints.products);
@@ -27,7 +26,47 @@ class ProductService {
         throw Exception('Failed to fetch products: ${e.message}');
       }
     } catch (e) {
-       throw Exception('Unexpected error: $e');
+      throw Exception('Unexpected error: $e');
     }
-  }  
+  }
+
+  Future<Product> getProductById(int id) async {
+    try {
+      final resp = await _dio.get(ApiEndpoints.productDetail(id));
+
+      if (resp.statusCode == 200) {
+        return Product.fromJson(resp.data as Map<String, dynamic>);
+      } else {
+        throw Exception('Failed to load product detail');
+      }
+    } on DioException catch (e) {
+      if (e.type == DioExceptionType.connectionTimeout) {
+        throw Exception('Connection timeout');
+      } else if (e.type == DioExceptionType.connectionError) {
+        throw Exception('No internet connection');
+      } else {
+        throw Exception('Failed to fetch products: ${e.message}');
+      }
+    }
+    catch (e) {
+      throw Exception('Unexpected error: $e');
+    }
+  }
+
+  Future<List<Product>> searchProducts(String query) async {
+    try {
+      final allProducts = await getProducts();
+      
+      if (query.isEmpty) {
+        return allProducts;
+      }
+      
+      return allProducts
+          .where((product) =>
+              product.title.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    } catch (e) {
+      throw Exception('Failed to search products: $e');
+    }
+  }
 }
