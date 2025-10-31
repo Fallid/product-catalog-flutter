e2b53436e387ca94e61bc64037129434a7f3136a
Ahmad Naufal <naufalopal002@gmail.com>
2025-10-31 22:38:55 +0700

    feat(main): init Hive setup every app launched

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:product_catalog/core/services/cart_service.dart';
 import 'package:product_catalog/core/utils/dimension/screen_dimension.dart';
+import 'package:product_catalog/features/cart/application/cart_provider.dart';
 import 'package:product_catalog/features/products/presentation/product_list_screen.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Init Hive
+  final cartService = CartService();
+  await cartService.init();
+  
   runApp(
     ProviderScope(
+      overrides: [
+        cartServiceProvider.overrideWithValue(cartService),
+      ],
       child: ScreenUtilInit(
         minTextAdapt: true,
         splitScreenMode: false,
-        builder: (context, child) => MyApp(),
+        builder: (context, child) => const MyApp(),
       ),
     ),
   );
